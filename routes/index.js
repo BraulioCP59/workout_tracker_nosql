0af967c5ee9667febe04515da37c870f35eeb264
Braulio Mora <brauliocdc59@gmail.com>
2022-02-08 22:39:28 -0800

    added a route to server the stats.html page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const exercise_page = require('./exercise');
+const stats_page = require('./stats');
 
 router.use('/exercise', exercise_page);
-
-
-//
-
+router.use('/stats', stats_page);
 
 
 
